a4dba9dd5e45d4a978e36b2aec9bd97831950d13
岡本 翔真 <s-okamoto@i-enter.co.jp>
2023-01-11 18:00:42 +0900

    react-hook-formでformを実装

--- fetch-user-info/src/Common/Input.tsx
+++ fetch-user-info/src/Common/Input.tsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import getUserInfo from "../ApexLegendsStatus";
 
 interface InputProps {
-  label: string;
+  apiKey: string;
+  userName: string;
+  platform: string;
 }
 
-const Input: React.FC<InputProps> = (props) => {
+const Input: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputProps>();
+
+  const onSubmit: SubmitHandler<InputProps> = (data) => getUserInfo();
+
   return (
     <div>
-      <label>{props.label}</label>
-      <input type="text" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>API Key</label>
+          <input {...register("apiKey")} />
+        </div>
+        <div>
+          <label>User Name</label>
+          <input {...register("userName")} />
+        </div>
+        <div>
+          <label>Platform</label>
+          <input {...register("platform")} />
+        </div>
+        <input type="submit" />
+      </form>
     </div>
   );
 };
--- fetch-user-info/src/TestPage.tsx
+++ fetch-user-info/src/TestPage.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Input from "./Common/Input";
+import { useForm, SubmitHandler } from "react-hook-form";
 import getUserInfo from "./ApexLegendsStatus";
 import style from "./Resources/styles/testPage.module.scss";
 
+interface Inputs {
+  apiKey: string;
+  userName: string;
+  platform: string;
+}
+
 const TestPage: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) =>
+    console.log("onSubmit: ", data);
+
   return (
     <div className={style.testPage}>
       <div className={style.wrapper}>
@@ -11,14 +27,12 @@ const TestPage: React.FC = () => {
         <div className={style.container}>
           <h2>必須パラメータ</h2>
           <div className={style.apiItems}>
-            <Input label="Api Key" />
-            <Input label="User Name" />
-            <Input label="Platform" />
-            <button onClick={getUserInfo}>検索</button>
+            <Input />
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default TestPage;
