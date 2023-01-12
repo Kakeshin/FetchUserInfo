1c86a5142e7ec318b077ad758e4e3b151d071690
岡本 翔真 <s-okamoto@i-enter.co.jp>
2023-01-12 10:47:20 +0900

    Löschen Sie unnötige Codes.

--- fetch-user-info/src/TestPage.tsx
+++ fetch-user-info/src/TestPage.tsx
@@ -1,25 +1,8 @@
 import React from "react";
-import Input from "./Common/Input";
-import { useForm, SubmitHandler } from "react-hook-form";
-import getUserInfo from "./ApexLegendsStatus";
+import Input from "./Common/Form";
 import style from "./Resources/styles/testPage.module.scss";
 
-interface Inputs {
-  apiKey: string;
-  userName: string;
-  platform: string;
-}
-
 const TestPage: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
-
-  const onSubmit: SubmitHandler<Inputs> = (data) =>
-    console.log("onSubmit: ", data);
-
   return (
     <div className={style.testPage}>
       <div className={style.wrapper}>
